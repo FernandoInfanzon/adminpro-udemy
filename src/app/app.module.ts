2584d6159f77390f9837f32ff2608dd15fcfc063
Fernando Infanzon <luisfernandoim@gmail.com>
2018-03-01 21:53:48 -0600

    crear 404 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule
