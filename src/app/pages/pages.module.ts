42d397a9e6fffa09aa3f55f991133da8bf8dba16
Fernando Infanzon <luisfernandoim@gmail.com>
2018-05-15 17:15:56 -0500

    Input, Output y ViewChild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,19 +1,28 @@
-
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { PagesComponent } from './pages.component';
 
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
+import {GraficoDonaComponent} from '../components/grafico-dona/grafico-dona.component';
+
+import {FormsModule} from '@angular/forms';
+
 import { SharedModule } from './../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 
 @NgModule({
     declarations: [
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-        PagesComponent
+        PagesComponent,
+        IncrementadorComponent,
+        GraficoDonaComponent
+
     ],
     exports: [ // si no se van a usar dentro del modulo no se tiene que exportar
         DashboardComponent,
@@ -23,7 +32,10 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
+
     ]
 })
 
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+progreso1: number = 10;
+progreso2: number = 90;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  // actualizar(event){
+  //   console.log('Evento', event);
+  // }
 
 }
